7f0ebab69174f08dfb0a1c1a73a0ddc4aee40735
eternalbeginner <ini.dwiii@gmail.com>
2022-06-02 12:03:55 +0800

    feat(helper): ❤️ add helper for import json data
    Add helper tool for importing json data from the databases folder

--- app/utilities/helper.py
+++ app/utilities/helper.py
@@ -1,3 +1,5 @@
+from json import load
+from os import path
 from pyrogram.types.messages_and_media import Message
 from typing import List, Tuple
 from app.libraries import env, log
@@ -18,3 +20,21 @@ def get_command_data(message: Message) -> Tuple[str, List[str]]:
   arguments = message_segments[0].split(" ") if len(message_segments) != 0 else []
 
   return (command, arguments)
+
+
+def import_json_data(name: str):
+  db_name = "data-{}.json".format(name.lower())
+  db_path = path.join("app/databases", db_name)
+
+  try:
+    with open(db_path, mode="r") as db_file:
+      return load(db_file)
+  except BufferError as e:
+    log.error("Buffer exception occured", e)
+    pass
+  except Exception as e:
+    log.error("Exception occured", e)
+    pass
+
+  return {}
+
